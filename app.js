5c37b2b9b971b4f701fb6e98fe702b08904c8e7f
patrickruk <patrickruk39@gmail.com>
2026-06-09 14:50:26 +0200

    Day6 Blog API with login + roles

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
-const express = require("express");
-const app = express();
-app.use(express.json()); // Middleware to parse JSON
+const bcrypt = require("bcrypt");
 
-// In-memory data store
-let posts = [
-  { id: 1, title: "First Post", content: "Hello world!" },
-];
-
-// GET all posts
-app.get("/posts", (req, res) => {
-  res.json(posts);
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: {type: String, enum:["admin","editor","user"], default: "user"}
 });
 
-// GET single post by ID
-app.get("/posts/:id", (req, res) => {
-  const post = posts.find(p => p.id == req.params.id);
-  post ? res.json(post) : res.status(404).send("Post not found");
+bcrypt.hash(req.body.password, 10, (err, hash) => {
+  const newUser = new User({ username: req.body.username,password: hash, role: req.body.role }); 
+  newUser.save();   
 });
-
-// POST new post
-app.post("/posts", (req, res) => {
-  const newPost = { id: posts.length + 1, ...req.body };
-  posts.push(newPost);
-  res.status(201).json(newPost);
-});
-
-// PUT update post
-app.put("/posts/:id", (req, res) => {
-  const post = posts.find(p => p.id == req.params.id);
-  if (post) {
-    post.title = req.body.title || post.title;
-    post.content = req.body.content || post.content;
-    res.json(post);
-  } else {
-    res.status(404).send("Post not found");
+ 
+bcrypt.compare(hash, req.body.password, (err, result) => {
+  if (!result) return res.status(401).json({ message: "Authentication failed" });
+  else {
+       const token = jwt.sign({ userId: user._id, role: user.role }, "secretKey", { expiresIn: "1h" });
   }
+ });
+
+function authMiddleware(req,res,next) {
+
+  const authHeader = req.header.authorization;
+  if(!authHeader) return res.status(401).send("Failed to have jwt");
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token,bcrypt.hash,(err,decoded) => {
+  if(err) return res.status(401).send("invalid token");
+  
+  req.user = decoded;
+  next();  
+})
+};
+
+function roleMiddleware(role){
+
+  return (req,res,next) => { 
+  if(!req.user) return res.send(403).send("not authenticated");
+
+  if(req.user.role !== user.role || user.admin){
+  return res.status(403).send({message: "not allowed"});
+  }
+  next();
+  };
+}
+
+app.get("admin",authMiddleware,roleMiddleware("admin"),(req,res) => {
+  res.send("welcome admin" + req.user.name);
 });
 
-// DELETE post
-app.delete("/posts/:id", (req, res) => {
-  posts = posts.filter(p => p.id != req.params.id);
-  res.send("Post deleted");
+app.get("admin",authMiddleware,roleMiddleware("user"),(req,res) => {
+  res.send("welcome user" + req.user.name);
 });
 
-app.listen(3000, () => {
-  console.log("Blog API running on http://localhost:3000");
+app.get("admin",authMiddleware,roleMiddleware("editor"),(req,res) => {
+  res.send("welcome editor" + req.user.name);
 });
+
+app.post("/posts", authMiddleware, roleMiddleware("editor"), createPost);
+
+app.delete("/posts/:id", authMiddleware, roleMiddleware("admin"), deletePost);
